756e00a73e537f43ac68d921d1fdf223bf8f26e6
Grace <tl544@cornell.edu>
2024-03-10 05:42:31 -0400

    Login and homepage

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { RouterProvider } from "react-router-dom";
+// import router from "./router";
+import Auth from "./Auth.js";
+import Homepage from "./Homepage.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <RouterProvider router={router}></RouterProvider>
+
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Auth />} />
+          <Route path="/homepage" element={<Homepage />} />
+        </Routes>
+      </div>
+    </Router>
+
+    // <div className="App">
+    //   <Auth />
+    // </div>
   );
 }
 
